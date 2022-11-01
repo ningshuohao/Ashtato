f69c67c0d15d71969f1a9c825a05bb8cadb409ac
ningshuohao <ningshuohao@163.com>
2022-11-01 20:57:37 +0800

    Update application.cpp

--- code/application.cpp
+++ code/application.cpp
@@ -7,25 +7,6 @@
 using namespace std;
 using namespace shio;
 
-//此函数用strtok方法
-string StringSplit(string TODO,string SPLITSTR,int TODOCHARNUM){
-    //获得Char型的被分隔值
-    char* toDoChar = (char*)TODO.c_str();
-    //获得Char型的分隔值
-    const char* splitStrChar = SPLITSTR.c_str();
-    //隔出后的暂存点
-    char* token;
-    int tokenCharNum;
-    //分割
-    //第一个
-    token = strtok(toDoChar,splitStrChar);
-    //剩下的
-    for (int i = 1;i < TODOCHARNUM;i++){
-        token = strtok(NULL,splitStrChar);
-    };
-    string RETURNSTR = string(token);
-    return RETURNSTR;
-};
 int main(){
     string appMode;
     cout << "Ashto,version 0.1" << endl << "Mode 1:Open file to use." << endl << "Mode 2:Enter command." << endl;
